89dd1e0edc68100393a36dba88c3c019149e09f6
Salauddin Sajeebe <salauddin15-749@diu.edu.bd>
2022-08-02 01:35:09 +0600

    interval

--- app/models/db.js
+++ app/models/db.js
@@ -6,31 +6,18 @@ const con = mysql.createConnection({
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
+
 });
-
-function handleDisconnect() {
-  // Recreate the connection, since
-  // the old one cannot be reused.
-  con.connect(function (err) {              // The server is either down
-    if (err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }
-    else if (!err) {
-      console.log("DB connection succeeded.");
-    }                                            // to avoid a hot loop, and to allow our node script to
-  });                                         // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
-  con.on('error', function (err) {
-    console.log('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
+con.connect((err) => {
+  if (!err) {
+    console.log("DB connection succeeded.");
+  } else
+    console.log(
+      "DB connection failed \n Error : " + JSON.stringify(err, undefined, 2)
+    );
+});
+setInterval(function () {
+  con.query('SELECT 1');
+}, 5000);
 
 module.exports = con;
